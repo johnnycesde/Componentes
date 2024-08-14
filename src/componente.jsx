b4fbe36604ba8670ad3d4dd39a3bcec87636747d
jonny londoño <johnnylon76@gmail.com>
2024-08-13 20:14:28 -0500

    se crea primer componente para estructurar el ejercicio

---
+++ src/componente.jsx
@@ -0,0 +1,13 @@
+function Componente(){
+       return(
+        <main>
+            <h1>hola johnny</h1>
+            <h1>hola johnny</h1>
+            
+            <input type="color"/>
+            <a href="http://"></a>
+        </main>
+       )
+}
+
+export default Componente
